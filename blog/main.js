84e37b558ed6740b4fc4e127d779a7e34a4d6707
the349 <josephmalone25@gmail.com>
2018-04-30 17:12:38 -0400

    Update main.js

--- blog/main.js
+++ blog/main.js
@@ -1,6 +1,6 @@
 let feed;fetch("../feed.json").then(data => {data.json().then(data => {feed = data})}).then(() => {
     for(var i = 0;i < feed.posts.length;i++) {
-        $('#posts-box').html("<h1>" + feed.posts[i].blogTitle + "</h1>" + "<p>" + feed.posts[i].blogText + "</p>"+ $('#posts-box').html())
+        $('#posts-box').html("<div>" + feed.posts[i].blogHead + "</div>" + "<h1>" + feed.posts[i].blogTitle + "</h1>" + "<p>" + feed.posts[i].blogText + "</p>"+ $('#posts-box').html())
     }
 })
 $('#page').addClass('slideInDown')
