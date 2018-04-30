17313f0723c5d2ad19adecce50c564620dd66c89
the349 <josephmalone25@gmail.com>
2018-04-30 17:05:10 -0400

    Update main.js

--- blog/main.js
+++ blog/main.js
@@ -1,5 +1,5 @@
 let feed;fetch("../feed.json").then(data => {data.json().then(data => {feed = data})}).then(() => {
-    for(var i = 0;i > feed.posts.length;i++) {
+    for(var i = 0;i < feed.posts.length;i++) {
         $('#posts-box').html("<h1>" + feed.posts[i].blogTitle + "</h1>" + "<p>" + feed.posts[i].blogText + "</p>"+ $('#posts-box').html())
     }
 })
