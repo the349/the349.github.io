69e1f5d45e5d41344da2fe7a47b9f8575f40247d
the349 <josephmalone25@gmail.com>
2018-05-07 15:01:16 -0400

    Update main.js

--- blog/main.js
+++ blog/main.js
@@ -4,5 +4,3 @@ let feed;fetch("../feed.json").then(data => {data.json().then(data => {feed = da
     }
 })
 $('#page').addClass('slideInDown')
-//AJAX animation things
-$(function(){"use strict";var t={prefetch:!0,cacheLength:2,onStart:{duration:250,render:function(t){t.addClass("slideOutDown"),n.restartCSSAnimations()}},onReady:{duration:0,render:function(t,n){t.removeClass("slideOutDown"),t.html(n)}}},n=$("#page").smoothState(t).data("smoothState")});
